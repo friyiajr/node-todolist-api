c0d361ada56eff077762acb511ceebcba595c449
Daniel Friyia <friyia@outlook.com>
2017-08-14 14:58:13 -0400

    Some playgrounding with databse insertions

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,35 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+	if(err) {
+		console.log('Connection Failure');
+		return;
+	}
+	console.log('Database Connection is live!');
+
+	// db.collection('Todos').insertOne({
+	// 	text: "Something to do",
+	// 	completed: false
+	// }, (err, result) => {
+	// 	if(err) {
+	// 		console.log('The insertion failed', err)
+	// 		return;
+	// 	}
+	// 	console.log(JSON.stringify(result.ops, undefined, 2));
+	// });
+
+	//Insert new doc (name, age, location)
+	db.collection('Users').insertOne({
+		name: "Dan Friyia",
+		age: 25,
+		location: "Waterloo, Ontario"
+	}, (err, result) => {
+		if(err) {
+			return console.log("Unable to insert");
+		}
+		console.log(JSON.stringify(result.ops, undefined, 2));
+	});
+
+
+	db.close();
+});
