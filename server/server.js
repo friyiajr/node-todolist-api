21295079f42a9a39b9984466c3017cc2b8fc9653
Daniel Friyia <friyia@outlook.com>
2017-08-15 18:05:29 -0400

    Some refactoring took place, express intialized

--- server/server.js
+++ server/server.js
@@ -1,52 +1,29 @@
-const mongoose = require('mongoose');
+// Library Imports
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// Custom Imports
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/Todo');
+var {User} = require('./models/User')
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    console.log(e);
+    res.status(400).send(e);
+  });
 });
 
-// User model
-// email - required, trimmed, type: string, minlength: 1
-var User = mongoose.model('User', {
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 1
-  }
-});
-
-// var otherTodo = new Todo({
-//   text: '  Edit this code  '
-// })
-//
-// otherTodo.save().then((doc) => {
-//   console.log(doc);
-// }, (e) => {
-//   console.log(e);
-// });
-
-var usr = new User({
- email: "e@e.com"
-});
-
-usr.save().then((doc) => {
-  console.log(doc);
-}, (err) => {
-  console.log(err);
+app.listen(3000, () => {
+  console.log("Started on port 3000");
 });
