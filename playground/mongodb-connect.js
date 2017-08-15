f2f9f3da911f61808b8de7cc8415b5cfb58de7a8
Daniel Friyia <friyia@outlook.com>
2017-08-15 14:12:08 -0400

    More Playgrounding

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 	if(err) {
@@ -19,17 +20,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 	// });
 
 	//Insert new doc (name, age, location)
-	db.collection('Users').insertOne({
-		name: "Dan Friyia",
-		age: 25,
-		location: "Waterloo, Ontario"
-	}, (err, result) => {
-		if(err) {
-			return console.log("Unable to insert");
-		}
-		console.log(JSON.stringify(result.ops, undefined, 2));
-	});
-
+	// db.collection('Users').insertOne({
+	// 	name: "Dan Friyia",
+	// 	age: 25,
+	// 	location: "Waterloo, Ontario"
+	// }, (err, result) => {
+	// 	if(err) {
+	// 		return console.log("Unable to insert");
+	// 	}
+	// 	console.log(result.ops[0]._id.getTimestamp());
+	// });
 
 	db.close();
 });
