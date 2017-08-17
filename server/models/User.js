ec3280391d7f0c3cd422871e2fa18c5b57d6ab9c
Daniel Friyia <friyia@outlook.com>
2017-08-17 15:24:12 -0400

    Added some functionality for creating a user

--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,35 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 var User = mongoose.model('Users', {
   email: {
     type: String,
     required: true,
     trim: true,
-    minlength: 1
-  }
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: (value) => {
+        return validator.isEmail(value);
+      },
+      message: '{VALUE} email is invalid'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
 
 module.exports = {
